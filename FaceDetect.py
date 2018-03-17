7003409183191173687ee4e759755dca815ece33
Mashnoon Islam <mashnoon1234@gmail.com>
2018-03-17 23:34:02 +0600

    Added pre-trained models and also added lbp face detector. The code is fully working.

--- FaceDetect.py
+++ FaceDetect.py
@@ -5,6 +5,8 @@ class FaceDetect: # This class contains all detection algorithms encapsulated in
         self.__name = name
         if(self.__name == "haar"):
             self.__cascade = cv2.CascadeClassifier(xmlOrCfg)
+        elif(self.__name == "lbp"):
+            self.__cascade = cv2.CascadeClassifier(xmlOrCfg)
         elif(self.__name == "yolo2"):
             self.__cfg = xmlOrCfg
             self.__weights = weights
@@ -13,6 +15,8 @@ class FaceDetect: # This class contains all detection algorithms encapsulated in
     def detect(self, frame): # This function reads the command-line arguments and decides which algorithm to use
         if(self.__name == "haar"):
             return self.__detectHaarcascade(frame)
+        elif(self.__name == "lbp"):
+            return self.__detectLbpcascade(frame)
         elif(self.__name == "yolo2"):
             return self.__detectYolo2()
 
@@ -21,6 +25,13 @@ class FaceDetect: # This class contains all detection algorithms encapsulated in
         for (x, y, w, h) in detectedFaces: # Iterates through the detected faces
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2) # Draws rectangles around detected faces
         return frame
+
+    def __detectLbpcascade(self, frame): # Local Binary Patterns Face Detection
+        detectedFaces = self.__cascade.detectMultiScale(frame, 1.1, 10) # Receives detected faces as an object
+        for (x, y, w, h) in detectedFaces: # Iterates through the detected faces
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2) # Draws rectangles around detected faces
+        return frame
+
 
     def __detectYolo2(self): # Yolo2 Face Detection
         pass
