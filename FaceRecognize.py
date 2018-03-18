ce8a0ee0ce506fdadc91ba2eee9eed2097a42b7e
Mashnoon Islam <mashnoon1234@gmail.com>
2018-03-19 02:19:37 +0600

    LBPH recognition partially done

--- FaceRecognize.py
+++ FaceRecognize.py
@@ -1,6 +1,15 @@
+import cv2
+
 class FaceRecognize: # This class contains all recognition algorithms encapsulated in functions
-    def __init__(self): Constructor / Initializer
-        pass
+    def __init__(self, name, xml): Constructor / Initializer
+        self.__name = name
+        if(self.__name == "lbph"):
+            self.__recognizer = cv2.face.createLBPHFaceRecognizer()
 
     def recognize(self): # This function reads the command-line arguments and decides which algorithm to use
-        pass
+        if(self.__name == "lbph"):
+            return self.__recognizeLbph(frame)
+
+    def __recognizeLbph(self, frame, detectedFaces):
+        for eachFace in detectedFaces:
+            self.__recognizer.predict(eachFace)
--- MachineLearning.py
+++ MachineLearning.py
@@ -1,3 +1,5 @@
+import numpy
+
 class MachineLearning: # This class contains training and testing algorithms of machine-learning algorithms encapsulated in functions
     def __init__(self): # Constructor / Initializer
         pass
@@ -7,6 +9,10 @@ class MachineLearning: # This class contains training and testing algorithms of 
 
     def testHaarcascade(self): # Tests Haarcascade Classifiers
         pass
+    
+    def trainLBPH(self, images, recognizer, imageLabels):
+        recognizer.train(images, numpy.array(imageLabels))
+        return recognizer
 
     def trainYolo2(self): # Trains Yolo2s
         pass
